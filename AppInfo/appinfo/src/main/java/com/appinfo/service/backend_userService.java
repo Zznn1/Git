ec285964a86a1bf9c292985bb85ab8ab4a778238
Zzn <903049865@qq.con>
2019-12-23 11:03:42 +0800

    APPInfo工程删除

--- AppInfo/appinfo/src/main/java/com/appinfo/service/backend_userService.java
+++
@@ -1,8 +0,0 @@
-package com.appinfo.service;
-
-import com.appinfo.entity.backend_user;
-
-public interface backend_userService {
-    //登录的方法
-    public backend_user login(backend_user user);
-}
--- AppInfo/appinfo/src/main/java/com/appinfo/service/dev_userService.java
+++
@@ -1,8 +0,0 @@
-package com.appinfo.service;
-
-import com.appinfo.entity.dev_user;
-
-public interface dev_userService {
-    //登录的方法
-    public dev_user login(dev_user user);
-}
